2ca968bcada4c70e97c3dfc0d2d1d067b661ce93
lynxphg98 <emtrai2k4@gmail.com>
2026-03-22 19:55:23 +0800

    Delete src directory

--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Alert } from './Alert';
-export { Skeleton, StoryCardSkeleton } from './Skeleton';
-export { Input } from './Input';
-export { TextArea } from './TextArea';
--- src/json.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.json" {
-  const value: unkown;
-  export default value;
-}
